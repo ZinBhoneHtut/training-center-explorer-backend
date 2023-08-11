8d5533b649e7151f5bacc66a411fd4d82d215420
SIPDOMAIN\ZinBhoneHtut <zinbhonehtut.sip@gmail.com>
2023-08-11 11:55:29 +0630

    refactor (datatable) : change records filtered count

--- src/main/java/com/zbh/tce/controller/BaseController.java
+++ src/main/java/com/zbh/tce/controller/BaseController.java
@@ -7,7 +7,7 @@ class BaseController {
     protected <T> DataTablesOutput<T> createDataTableOutput(Page<T> data, Long totalCount) {
         DataTablesOutput<T> dataTableOutput = new DataTablesOutput<T>();
         dataTableOutput.setRecordsTotal(totalCount);
-        dataTableOutput.setRecordsFiltered(data.getTotalElements());
+        dataTableOutput.setRecordsFiltered(data.getContent().size());
         dataTableOutput.setData(data.getContent());
         return dataTableOutput;
     }

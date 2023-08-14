fa9cfb7742d7333761e4a82314cd3b6d41052890
SIPDOMAIN\ZinBhoneHtut <zinbhonehtut.sip@gmail.com>
2023-08-14 14:15:03 +0630

    remove (constant) : remove unused java file

--- src/main/java/com/zbh/tce/common/constant/AuthoritiesConstant.java
+++
@@ -1,6 +0,0 @@
-package com.zbh.tce.common.constant;
-
-public class AuthoritiesConstant {
-	public static final String SUPER_ADMIN = "ROLE_SUPER_ADMIN";
-	public static final String ADMIN = "ROLE_ADMIN";
-}
